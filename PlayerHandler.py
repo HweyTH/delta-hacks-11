931231f17f25a4ad4d42a53c794a7f1796185036
aiden10 <aiden.ten30@gmail.com>
2025-01-11 17:44:46 -0500

    update handler to include more game logic

--- PlayerHandler.py
+++ PlayerHandler.py
@@ -1,33 +1,38 @@
 import json
+from functions import get_words
 
 class PlayerHandler():
     def __init__(self, duration):
         self.duration = duration
         self.time = 1
-        self.index = 0
-        self.all_wpm = [] # add wpm to list each second
+        self.letter_index = 0
+        self.word_index = 0
+        self.all_wpm = [] # Add wpm to list each second
         self.current_wpm = 0
         self.highest_wpm = 0
-        self.accuracy = 0
-        self.mistakes = 0 # count
+        self.mistakes = 0 # Total count of mistakes made
         self.words_typed = []
-        self.current_string = None
+        self.current_string = get_words(10000) # Get a bunch of words so we don't have to keep fetching more
+        self.current_word = self.current_string[0]
+        self.total_inputs = 1
 
     # Call every second
     def update_wpm(self):
+        self.time += 1 # Should maybe be in its own place
         self.current_wpm = self.current_wpm / (self.time/60)
         self.all_wpm.append(self.current_wpm)
         if self.current_wpm > self.highest_wpm: self.highest_wpm = self.current_wpm
 
     def save_stats(self):
-        avg_wpm = sum(self.all_wpm) / self.duration # divide by duration to get average wpm
+        avg_wpm = sum(self.all_wpm) / self.duration # Divide by duration to get average wpm
+        accuracy = self.mistakes / self.total_inputs
         old_data = json.load(open('data/scores.json', 'r'))
         old_data['games_played'] += 1
         stats = {
             old_data['games_played']: {
             'avg_wpm': avg_wpm,
             'highest_wpm': self.highest_wpm,
-            'accuracy': self.accuracy,
+            'accuracy': accuracy,
             'mistakes': self.mistakes,
             'words_typed': self.words_typed
             }
@@ -38,3 +43,31 @@ class PlayerHandler():
         
         print('saved data')
 
+    """
+    Returns True if detected letter matches the letter that needs to be typed, returns False if it does not.
+    Returns None if detected letter is delete.
+    """
+    def process_frame(self, frame):
+        detected_letter = detect_letter(frame) # Replace with the actual letter detection function 
+        if detected_letter:
+            self.total_inputs += 1
+
+
+        # Finished typing word
+        if self.letter_index == len(self.current_word) - 1 and detected_letter == self.current_word[self.letter_index]:
+            # Only move to the next word if the current word was typed correctly
+            self.words_typed.append(self.current_word)
+            self.word_index += 1
+            self.current_word = self.current_string[self.word_index]
+
+        if detected_letter == 'delete':
+            self.letter_index -= 1
+            return None
+        
+        self.letter_index += 1
+        if self.current_string[self.letter_index] == detected_letter:
+            return True
+        else:
+            self.mistakes += 1
+            return False
+    
